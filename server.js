3c9929d736f0920b14b463032db7ff65e4f681a0
Kunal Tankra <kunaltankra2002@gmail.com>
2024-10-23 20:08:23 +0530

    changed db

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.listen(process.env.PORT, () => {
     console.log('app is listening on port:', process.env.PORT)
 
     // connect with db
-    mongoose.connect(process.env.MONGO_URI, { dbName: 'newOne' }).then(() => console.log('db connected'))
+    mongoose.connect(process.env.MONGO_URI, { dbName: 'TODO' }).then(() => console.log('db connected'))
         .catch((err) => console.log('error connecting with db:', err))
 })
